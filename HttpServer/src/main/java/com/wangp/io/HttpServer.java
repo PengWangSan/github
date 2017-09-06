fcd0e0a00431bef7c40e5dae9d6f7a66a78c6602
pengwang20170820 <wswpye@163.com>
2017-09-06 22:06:39 +0800

    nio
    nio

---
+++ HttpServer/src/main/java/com/wangp/io/HttpServer.java
@@ -0,0 +1,38 @@
+package com.wangp.io;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+
+	public static void main(String[] args) {
+
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(80);
+
+			while (true) {
+				Socket socket = server.accept();
+				
+				System.out.println("request:"+socket+"connectd");
+				
+//				ServerThread t=new ServerThread(socket);
+				executorService.submit(new ServerThread(socket));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				server.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ HttpServer/src/main/java/com/wangp/io/ServerThread.java
@@ -0,0 +1,84 @@
+package com.wangp.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+
+	private Socket socket;
+
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+
+	public void run() {
+
+		try {
+			InputStreamReader reader = new InputStreamReader(socket.getInputStream());
+
+			LineNumberReader lineReader = new LineNumberReader(reader);
+
+			String lineStr = null;
+
+			String requestPage = null;
+			while (true) {
+				
+				System.out.println("新数据可以读取：");
+				while ((lineStr = lineReader.readLine()) != null) {
+
+					System.out.println(lineStr);
+					if (lineReader.getLineNumber() == 1) {
+
+						requestPage = lineStr.substring(lineStr.indexOf("/") + 1, lineStr.lastIndexOf(" "));
+
+					} else {
+						if (lineStr.isEmpty()) {
+							System.out.println("Head Finished");
+							doResponse(requestPage);
+							break;
+						}
+
+					}
+
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void doResponse(String requestPage) throws IOException {
+
+		String webRoot = "D:";
+		File file = new File(webRoot, requestPage);
+
+		OutputStream out = socket.getOutputStream();
+
+		if (file.exists()) {
+
+			out.close();
+
+		} else {
+			String msg = "12345678";
+
+			String res = "HTTP/1.1 200 OK\r\n";
+			res += "Server: nginx\r\n";
+			res += "Content-Length:" + (msg.length()-1) + "\r\n";
+			res += "\r\n";
+
+			res += msg;
+			System.out.println(res);
+			out.write(res.getBytes());
+			out.flush();
+			// out.close();
+		}
+
+	}
+
+}
