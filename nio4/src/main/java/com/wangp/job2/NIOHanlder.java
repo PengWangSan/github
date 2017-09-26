af3893ce0fbec901537a46b6222a75804f4d33e7
wangpeng <wswpye@163.com>
2017-09-26 13:24:44 +0800

    nio

--- nio4/src/main/java/com/wangp/job2/NIOHanlder.java
+++ nio4/src/main/java/com/wangp/job2/NIOHanlder.java
@@ -1,5 +1,9 @@
 package com.wangp.job2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
@@ -16,9 +20,17 @@ public class NIOHanlder {
 
 	private ByteBuffer readByteBuffer;
 
+
 	private int lasMsgEndPosition = 0;
 
 	private final SelectionKey selectionKey;
+	
+	
+	private FileChannel fileChannel;
+	
+	private long fileStartPosition;
+	
+	private long fileEndPosition;
 
 	public NIOHanlder(SocketChannel socketChannel, Selector selector) throws IOException {
 		super();
@@ -26,8 +38,7 @@ public class NIOHanlder {
 		this.selector = selector;
 		socketChannel.configureBlocking(false);
 		selectionKey = socketChannel.register(selector, SelectionKey.OP_READ, this);
-		
-		
+
 		socketChannel.write(ByteBuffer.wrap("Welcome,My Dear boy\r\n".getBytes()));
 
 		readByteBuffer = ByteBuffer.allocate(1000);
@@ -49,45 +60,110 @@ public class NIOHanlder {
 					readByteBuffer.position(lasMsgEndPosition);
 					readByteBuffer.get(msg);
 					processReadDate(msg);
-					readByteBuffer.position(msgEndPosition+2);
+					readByteBuffer.position(msgEndPosition + 2);
 					readLine = true;
 				}
 			}
-		
+
 			if (readLine) {
 				readByteBuffer.limit(position);
 				readByteBuffer.compact();
-				lasMsgEndPosition=0;
+				lasMsgEndPosition = 0;
 
 			}
+		}else if(selectionKey.isWritable()) {
+			
+			
+			
+			fileStartPosition=fileEndPosition;
+			long length=fileChannel.transferTo(fileStartPosition, fileChannel.size(), socketChannel);
+			fileEndPosition=fileStartPosition+length-1;
+			if(fileEndPosition==fileChannel.size()-1) {
+				
+				
+				System.out.println("__End");
+				
+				selectionKey.interestOps(selectionKey.interestOps()&~SelectionKey.OP_WRITE);
+			}
+			System.out.println(fileEndPosition);
 		}
 
 	}
 
-	private void processReadDate(byte[] msg) {
+	private void processReadDate(byte[] msg) throws IOException {
 		System.out.println("rec msg:" + new String(msg));
+
+		String msgStr = new String(msg);
+
+		if (msgStr.contains("download")) {
+
+			String filePath = msgStr.replace("download", "").trim();
+
+			File file = new File(filePath);
+
+			if (!file.exists()) {
+
+				socketChannel.write(ByteBuffer.wrap("File not exist\r\n".getBytes()));
+				return;
+			}
+			if (file.isDirectory()) {
+				socketChannel.write(ByteBuffer.wrap("The Path is not file\r\n".getBytes()));
+				return;
+			}
+
+			fileChannel = new FileInputStream(file).getChannel();
+
+			fileStartPosition=0;
+			long length=fileChannel.transferTo(fileStartPosition, fileChannel.size(), socketChannel);
+			fileEndPosition=length+fileStartPosition-1;
+			if(fileEndPosition<=fileChannel.size()-fileStartPosition) {
+				selectionKey.interestOps(selectionKey.interestOps()|SelectionKey.OP_WRITE);
+				
+			}
+			
+			
 		
-		String msgStr=new   String(msg);
-		
-		if(msgStr.contains("download")) {
-			
-			String filePath=msgStr.replace("download", "").trim();
-			
-			
+
 		}
 		
-		
-		
-		
-		
-		
+		if(msgStr.equals("exit")) {
+			socketChannel.close();
+		}
+
 	}
 
-	public static void main(String[] s) {
+	public static void main(String[] s) throws IOException {
 
-		byte[] a = "\r".getBytes();
+		File file = new File("D:\\soft\\G2017_02_4.txt");
 
-		System.out.println(a.length);
+		FileInputStream fis = new FileInputStream(file);
+		int r = 0;
+
+		StringBuffer sb = new StringBuffer();
+		do {
+			byte[] content = new byte[1000];
+			r = fis.read(content);
+			sb.append(new String(content));
+		} while (r != -1);
+		fis.close();
+
+		System.out.println(sb);
+
+		FileOutputStream fos = new FileOutputStream("D:/nio4.txt");
+
+		for (int i = 1;; i++) {
+
+			fos.write((sb.toString()+"++++"+i).getBytes());
+			
+			if(sb.toString().getBytes().length*i>110*1024*1024) {
+				
+				break;
+			}
+			
+		}
+
+		fos.flush();
+		fos.close();
 	}
 
 }
